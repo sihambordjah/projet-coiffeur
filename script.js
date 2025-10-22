b802f4a087362abd09499e1a7466708cb87f42e3
siham bordjah <sihamsissa1903@gmail.com>
2025-10-22 21:49:58 +0200

    Premier commit - site coiffeur

---
+++ script.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll(".slide");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+// Changement automatique toutes les 5 secondes
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
